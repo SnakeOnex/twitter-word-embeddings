603c5939b0ce251ad563854d615100fb55b1a6b2
snakeone <romakcz@gmail.com>
2019-03-28 21:23:54 +0100

    plotting thing works Pog

--- app/client/src/components/app.js
+++ app/client/src/components/app.js
@@ -1,25 +1,45 @@
 import React from "react";
 import WordSearch from "./word-search";
 import EmbeddingTable from "./embedding-table";
+import Plot from "react-plotly.js";
 
 class App extends React.Component {
   state = {
-	embs: []
+	embs: {}
   }
 
   getEmbs = embs => {
 	this.setState({
 	  embs: embs,
-	  plot: {}
 	});
   }
 
+  getPlot() {
+	return (
+		<Plot
+        data={[
+          {
+			x: this.state.embs.X.map(x => { return parseFloat(x)}),
+			y: this.state.embs.Y.map(y => { return parseFloat(y)}),
+            type: 'scatter',
+			mode: 'markers+text',
+			text: this.state.embs.labels,
+			textposition: 'bottom-center',
+            marker: {color: 'red'},
+          },
+        ]}
+        layout={{xaxis: { range: [-50, 50]}, yaxis: { range: [-50, 50]}, width: 640, height: 480, title: 'Plotted words'} }
+      />
+	);
+  }
+
   render() {
-	console.log(this.state.embs.length == 0);
-	console.log(this.state.embs);
 	let rndr = "nothing";
-	if (this.state.embs.length > 0)
+	if (this.state.embs.words != null) {
 	  rndr = "similar";
+	  if (this.state.embs.X.length > 1)
+		rndr = "plot";
+	}
 	return (
 	  <div className="container">
 		<h2>Enter a word</h2>
@@ -27,8 +47,9 @@ class App extends React.Component {
 		  <WordSearch getEmbs={this.getEmbs} />
 		</div>
 		<div className="row">
-		  {rndr == "similar" ? <EmbeddingTable embs={this.state.embs} /> : null }
+		  {rndr == "similar" || rndr == "plot" ? <EmbeddingTable embs={this.state.embs.words} /> : null }
 		</div>
+		{rndr == "plot" ? this.getPlot() : null}
 	  </div>
 	);
   }
--- app/client/src/components/word-search.js
+++ app/client/src/components/word-search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import Plot from 'react-plotly.js';
 
 class WordSearch extends React.Component {
 
@@ -16,12 +16,11 @@ class WordSearch extends React.Component {
 
   handleClick = e => {
 	e.preventDefault();
-	console.log(e);
-	fetch("http://194.182.80.90:5000/" + this.state.inputValue) // Call the fetch function passing the url of the API as a parameter
+	let parsed = this.state.inputValue.split(',').map(s => s.trim()).join('+');
+	fetch("http://194.182.80.90:5000/" + parsed) // Call the fetch function passing the url of the API as a parameter
 	.then((response) => response.json())
 	.then((responseJSON) => {
-	  console.log(responseJSON.embs);
-	  this.props.getEmbs(responseJSON);
+	  this.props.getEmbs(JSON.parse(responseJSON));
 	})
 	.catch(function(error) {
 	  console.log(error);
